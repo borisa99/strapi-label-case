b21ab17008485e500bf6e42240d5fccd5cb1c166
Borisa Krstic <borisakrstic02@gmail.com>
2023-01-31 11:27:54 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,43 @@
+
+# Strapi Label Case
+
+A script that updates Strapi configuration, it converts collection list and edit metadata labels to label case. 
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`STRAPI_URL`
+
+`STRAPI_ADMIN_EMAIL`
+
+`STRAPI_ADMIN_PASSWORD`
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone git@github.com:borisa99/strapi-label-case.git
+```
+
+Go to the project directory
+
+```bash
+  cd strapi-lable-case
+```
+
+Install dependencies
+
+```bash
+  yarn install
+```
+
+Start the script
+
+```bash
+  COLLECTION_TYPE_UID="your-collection-type-uid" node
+```
+
--- index.js
+++ index.js
@@ -1,53 +1,82 @@
 import delve from "dlv";
 import dotenv from "dotenv";
 import axios from "axios";
-
 dotenv.config();
 
-const client = axios.create({
-  baseURL: process.env.STRAPI_URL,
-  headers: { "Content-Type": "application/json" },
-});
+try {
+  if (!process.env.COLLECTION_TYPE_UID)
+    throw new Error("COLLECTION_TYPE_UID env variable is required");
 
-// Call a login endpoint
-// /configuration API is nto accessible with API Key
-const { data: loginData } = await client.post("/admin/login", {
-  email: process.env.STRAPI_ADMIN_EMAIL,
-  password: process.env.STRAPI_ADMIN_PASSWORD,
-});
-const token = delve(loginData, "data.token", null);
+  const client = axios.create({
+    baseURL: process.env.STRAPI_URL,
+    headers: { "Content-Type": "application/json" },
+  });
 
-// Get current configuration
-// Params:
-// uid - collection-type uid
-const uid = "api::page.page"; // TEMP
-const { data: configurationData } = await client.get(
-  `/content-manager/content-types/${uid}/configuration`,
-  {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+  const toLabelCase = (str) =>
+    str
+      .replace(/([A-Z])/g, " $1")
+      .replace(/^./, (str) => str.toUpperCase().trim());
+
+  const isObjectEmpty = (objectName) => Object.keys(objectName).length === 0;
+
+  // Call a login endpoint
+  // /configuration API is nto accessible with API Key
+  const { data: loginData } = await client.post("/admin/login", {
+    email: process.env.STRAPI_ADMIN_EMAIL,
+    password: process.env.STRAPI_ADMIN_PASSWORD,
+  });
+  const token = delve(loginData, "data.token", null);
+
+  // Get current configuration
+  // Params:
+  // uid - collection-type uid
+  const uid = process.env.COLLECTION_TYPE_UID;
+  const { data: configurationData } = await client.get(
+    `/content-manager/content-types/${uid}/configuration`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const configuration = delve(configurationData, "data.contentType", null);
+  if (!configuration) throw new Error("Configuration not found");
+
+  // Required formatting for PUT request
+  delete configuration.uid;
+
+  Object.keys(configuration.metadatas).map((key) => {
+    const field = Object.assign({}, configuration.metadatas[key]);
+    delete field.edit.mainField;
+    delete field.list.mainField;
+
+    if (!isObjectEmpty(field.edit)) {
+      field.edit = { ...field.edit, label: toLabelCase(field.edit.label) };
+    }
+    if (!isObjectEmpty(field.list)) {
+      field.list = { ...field.list, label: toLabelCase(field.list.label) };
+    }
+    configuration.metadatas[key] = field;
+  });
+
+  // Update configuration
+  // Params:layouts, metadatas, components
+  await client.put(
+    `/content-manager/content-types/${uid}/configuration`,
+    configuration,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+} catch (error) {
+  if (error.config) {
+    const { baseURL, url, method } = error.config;
+    console.log(error.message, method, baseURL + url);
+  } else {
+    console.log(error.message);
   }
-);
-
-const configuration = delve(configurationData, "data.contentType", null);
-if (!configuration) return;
-
-// Required formatting for PUT request
-delete configuration.uid;
-delete configuration.metadatas.tags.list.mainField;
-delete configuration.metadatas.author.list.mainField;
-
-// Update configuration
-// Params:layouts, metadatas, components
-const response = await client.put(
-  `/content-manager/content-types/${uid}/configuration`,
-  configuration,
-  {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  }
-);
-
-console.log(response.data);
+  process.exit(1);
+}
